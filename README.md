e231fa9ffb70050992ffab680c0321e0c2a6da4d
bazla <dobr1nya@mail.ru>
2024-02-07 20:52:48 +0300

    feat: add server

---
+++ README.md
@@ -0,0 +1,22 @@
+# CRUD API
+
+### To test app you need:
+
+- Clone git repository
+
+```
+git clone https://github.com/victordobryi/nodejs-crud-api.git
+
+```
+
+- Change the branch to develop
+
+```
+git checkout develop
+```
+
+- Run app
+
+```
+npm run start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Start app development');
+import http from 'http';
+import 'dotenv/config';
+
+const PORT = process.env.PORT;
+
+const myServer = http.createServer((req, res) => {
+  res.write('Hello World!');
+  res.end();
+});
+
+myServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}. Go to http://localhost:${PORT}/`);
+});
